685aa0e89636271c8425f66ee9f82393f71e286e
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2023-12-26 16:20:45 +0100

    arangoDB Repository: major clean up of code, types, and functionality
    - .use() method was removed as it is unnecessary in the new version of .useOrCreateDatabase()
    - Two test methods were added, one for exporting back the env variables, the other to repristine the whole database
    - Modified the core output port to match these changes; a type parameter was added to the port to better handle the Database type all around
    - Modified DTOs to reflect changes; in particular, the Connection DTO
      for ArangoDB now returns both: the _system database, and the named
    database we created; the _system one is used in some cases to create and
    drop other databases
    - Improved testing for arangodb-repository
    - Removed duplicated .env.test file in the test folder, now there's a single one in the root of the project; note that the host port at test/docker-compose.yml  must manually match the ./.env.test port
    - Added a mini migration file to play, but it's not being used yet

--- src/lib/core/dto/arangodb-dto.ts
+++ src/lib/core/dto/arangodb-dto.ts
@@ -1,24 +1,36 @@
 import { BaseDTO } from '@/lib/sdk/dto'
 
 /**
- * ArangoDBInitDTO contains information about status of connecting and initializing ArangoDB
+ * ArangoDBInitDTO contains information about status of connecting and initializing ArangoDB. Returns both the database object and the system database object. The system database object has as name "_system" in all ArangoDB projects. To create or drop databases, we need to use the system database object.
  */
 export interface ArangoDBConnectionSuccessDTO<DB> extends BaseDTO {
-  status: 'success',
-  arangoDB: DB | undefined
+  status: 'success'
+  arangoDB: DB
+  arangoSystemDB: DB
 }
 
 /**
  * ArangoDBInitDTO contains information about status of connecting and initializing ArangoDB
  */
 export interface ArangoDBConnectionErrorDTO extends BaseDTO {
-  status: 'error',
-  errorType: string | undefined,
-  errorCode: number | undefined,
+  status: 'error'
+  errorType: string | undefined
+  errorCode: number | undefined
   errorMessage: string
 }
 
 export type ArangoDBConnectionDTO<DB> = ArangoDBConnectionSuccessDTO<DB> | ArangoDBConnectionErrorDTO
+
+/**
+ * ArangoDBEnvVariablesDTO contains the values of the environment variables required for ArangoDB
+ */
+export interface ArangoDBEnvVariablesDTO extends BaseDTO {
+  URL?: string
+  PORT?: number
+  DATABASE?: string
+  USERNAME?: string
+  PASSWORD?: string
+}
 
 /**
  * ArangoDBCollectionDTO contains information about status of creating a collection in ArangoDB
